0b26d42675828b184e930f6ddf4f9b81d920cc51
Joao da Silva <joaodasilvagithub@gmail.com>
2022-01-08 14:22:20 +0100

    Remove temporary fix for simple-shapes.js

--- examples/p5/simple-shapes.js
+++ examples/p5/simple-shapes.js
@@ -4,12 +4,6 @@ function setup() {
   // Create the canvas
   createCanvas(720, 400);
   background(200);
-}
-
-// TODO: originally this was all in the body of setup(),
-// but the scaling is off in that case.
-function draw() {
-  // TODO: the alpha transparency isn't working.
   // Set colors
   fill(204, 101, 192, 127);
   stroke(127, 63, 120);
